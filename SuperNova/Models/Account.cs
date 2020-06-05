069f5dbdcf4b87a50219dec95ae1826269bc8e50
Shuhab <chistiya@gmail.com>
2020-06-05 17:13:53 -0400

    Abiity to Add, Edit or Delete Account

--- SuperNova/Models/Account.cs
+++ SuperNova/Models/Account.cs
@@ -7,6 +7,7 @@ namespace SuperNova.Models
 {
     public class Account
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
--- SuperNova/Services/AccountService.cs
+++ SuperNova/Services/AccountService.cs
@@ -13,17 +13,17 @@ namespace SuperNova.Services
     public class AccountService : Account
     {
         public Login AuthorizationToken { get; set; }
-         
+
         public async Task<List<Account>> GetAccounts()
         {
             List<Account> Accounts = new List<Account>();
-            var loginid = new LoginService().GetAuthenticationToken(AuthorizationToken).Result.LoginId;
             try
             {
+                var loginid = new LoginService().GetAuthenticationToken(AuthorizationToken).Result.LoginId;
                 using (SqlConnection connection = new SqlConnection(Constants.CONNECTIONSTRING))
                 {
                     connection.Open();
-                    SqlCommand command = new SqlCommand("SELECT * from Accounts WHERE loginid = '" + loginid + "'", connection);
+                    SqlCommand command = new SqlCommand("SELECT * from Accounts WHERE loginid = '" + loginid + "' ORDER BY Name", connection);
                     var reader = await command.ExecuteReaderAsync();
                     var dataTable = new DataTable();
                     dataTable.Load(reader);
@@ -32,6 +32,7 @@ namespace SuperNova.Services
                     {
                         Account account = new Account()
                         {
+                            Id = (int)(dataTable.Rows[i]["Id"]),
                             Name = dataTable.Rows[i]["Name"].ToString(),
                             Url = dataTable.Rows[i]["Url"].ToString(),
                             UserName = dataTable.Rows[i]["UserName"].ToString(),
@@ -48,6 +49,78 @@ namespace SuperNova.Services
             }
 
             return Accounts;
+        }
+
+        public async Task<bool> UpdateAccount(Account account)
+        {
+            var retVal = false;
+            try
+            {
+                account.LoginId = new LoginService().GetAuthenticationToken(AuthorizationToken).Result.LoginId;
+                using (SqlConnection connection = new SqlConnection(Constants.CONNECTIONSTRING))
+                {
+                    connection.Open();
+                    if (account.Id == 0)
+                    {
+                        SqlCommand command = new SqlCommand(@"INSERT INTO Accounts(LoginId, Name,Url,UserName,Password,Comment) VALUES('" +
+                                                             account.LoginId + "','" +
+                                                             account.Name + "','" +
+                                                             account.Url + "','" +
+                                                             account.UserName + "','" +
+                                                             account.Password + "','" +
+                                                             account.Comment + "')",
+                                                             connection);
+                        await command.ExecuteNonQueryAsync();
+                    }
+                    else
+                    {
+                        SqlCommand command = new SqlCommand(@"UPDATE Accounts SET " +
+                                                             "Name = '" +
+                                                             account.Name +
+                                                             "', Url = '" +
+                                                             account.Url +
+                                                             "', UserName = '" +
+                                                             account.UserName +
+                                                             "', Password = '" +
+                                                             account.Password +
+                                                             "', Comment = '" +
+                                                             account.Comment +
+                                                             "' WHERE Id='" + account.Id + "' AND LoginId = '" + account.LoginId + "'",
+                                                             connection);
+                        await command.ExecuteNonQueryAsync();
+                    }
+                }
+                retVal = true;
+            }
+            catch (Exception ex)
+            {
+                retVal = false;
+            }
+
+            return retVal;
+        }
+
+        public async Task<bool> DeleteAccount(int id)
+        {
+            var retVal = false;
+            try
+            {
+                var LoginId = new LoginService().GetAuthenticationToken(AuthorizationToken).Result.LoginId;
+                using (SqlConnection connection = new SqlConnection(Constants.CONNECTIONSTRING))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("DELETE from Accounts WHERE Id=" + id, connection);
+                    await command.ExecuteNonQueryAsync();
+                    retVal = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                retVal = false;
+            }
+
+            return retVal;
+
         }
     }
 }
